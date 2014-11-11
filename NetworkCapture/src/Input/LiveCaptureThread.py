db8f24128a71087a04d5dfa5df7b75d8e57acfc0
Shubham <shubham@ubuntu.ubuntu-domain>
2014-11-11 23:00:57 +0530

    Stop live capture and get packet features added

--- NetworkCapture/src/Input/LiveCaptureThread.py
+++ NetworkCapture/src/Input/LiveCaptureThread.py
@@ -4,12 +4,20 @@ from scapy.all import *
 class LiveCaptureThread(threading.Thread):
 	def __init__(self, liveCaptureObj):
 		threading.Thread.__init__(self)
+		self.stop = threading.Event()
 		self.liveCaptureObj = liveCaptureObj
-	
+		self.flag = False
+
 	def run(self):
-		sniff(prn=self.recv)
+		try:
+			sniff(prn=self.recv)
+		except Exception:
+			pass
 
 	def recv(self, packet):
-			self.liveCaptureObj.packetList.append(packet)
-			self.liveCaptureObj.packetBuffer.append(packet)
-			print packet.summary()
+		if self.flag:
+			raise Exception('Stop Packet Capturing')
+		self.liveCaptureObj.packetList.append(packet)
+		self.liveCaptureObj.packetBuffer.append(packet)
+		print 'recv'
+		print packet.summary()
