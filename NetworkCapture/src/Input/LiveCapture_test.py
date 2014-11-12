cad6cc4367b51a2d71f9656c1686d6730b2372b0
anshuli-patil <anshulipatil8@gmail.com>
2014-11-12 10:53:58 +0530

    Correcting LiveCapture test and renaming file
    Instead of direct comparison of sent/received packets, changing to
    comparison of the layers of the packets

--- NetworkCapture/src/Input/LiveCapture_test.py
+++ NetworkCapture/src/Input/LiveCapture_test.py
@@ -8,13 +8,19 @@ class TestCapture(TestCase):
 		capture.startLiveCapture()
 		
 		packet1 = IP(dst="10.4.12.91")/ICMP()/"HelloWorld"
-		packet2 = IP(dst="10.1.99.2")/TCP(dport=23)
+		packet2 = IP(dst="10.4.12.91")/TCP(dport=23)
 		packets = [packet1, packet2]
 		for packet in packets
 			send(packet)
 		
 		capture.stopLiveCapture()
-		assertEquals(packets, capture.packetList)
+		
+		for packet in capture.packetList
+			assertEqual(packet[IP].dst, "10.4.12.91" )
+			
+		self.assertTrue(capture.packetList[0].hasLayer(ICMP))
+		self.assertEqual(23,capture.packetList[1].getLayer(TCP).dport)		
+		#self.assertEqual(packets, capture.packetList)
 		
 		self.assertRaises(Exception, send, packet1)
 if __name__ == '__main__':
