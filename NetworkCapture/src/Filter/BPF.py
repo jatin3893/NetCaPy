25731a62283139243bb6a42cf7e69107dd777690
Shubham <shubham@ubuntu.ubuntu-domain>
2014-11-12 14:23:10 +0530

    Filtering packets based on filter expression

--- NetworkCapture/src/Filter/BPF.py
+++ NetworkCapture/src/Filter/BPF.py
@@ -1,7 +1,30 @@
 from Filter import Filter
-
+from scapy.all import *
+import os
 #################################################################
 class BPF(Filter):
     def __init__(self):
         Filter.__init__(self)
-        print "BPF Filter Base Class"
+        self.filterExpression = ''
+        self.filteredPacketList = []
+        self.originalPacketList = []
+
+    def setFilterExpression(self, filterExpression):
+    	self.filterExpression = filterExpression
+
+    def getFilterExpression(self):
+    	return self.filterExpression
+
+    def setOriginalPacketList(self, packetList):
+    	self.originalPacketList = packetList
+
+    def getFilteredPacketList(self):
+    	return self.filteredPacketList
+
+    def filterPacketList(self):
+    	wrpcap("temp.pcap", self.originalPacketList)
+    	self.filteredPacketList = sniff(offline="temp.pcap", filter=self.filterExpression)
+    	os.remove("temp.pcap")
+
+
+
