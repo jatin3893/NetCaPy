83290e527318da05a075317dcdfc414c20fd75b6
Shubham Kankaria <shubhamkank@gmail.com>
2014-11-14 11:42:44 +0530

    Packet size graph feature added

---
+++ NetworkCapture/src/Analysis/PacketSizeGraph.py
@@ -0,0 +1,26 @@
+from Analysis import Analysis
+import matplotlib.pyplot as plt
+
+class PacketSizeGraph(Analysis):
+	def __init__(self):
+		Analysis.__init__(self)
+		self.packetList = []
+
+	def setPacketList(self, packetList):
+		self.packetList = packetList
+
+	def plotGraph(self):
+		countList = []
+		sizeList = []
+		count = 0
+
+		for packet in self.packetList:
+			countList.append(count)
+			count = count + 1
+			sizeList.append(len(packet))
+		
+		plt.plot(countList, sizeList, 'ro')
+		plt.xlabel('Packet Number')
+		plt.ylabel('Packet Size (bytes)')
+		plt.title('Paket Size Graph')
+		plt.show()
